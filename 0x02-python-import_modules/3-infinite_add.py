ab3cdd9b6e9b481c9902de858a5d1db736e060c9
BrianOti <briancotieno@gmail.com>
2022-09-22 11:23:45 -0700

    task three

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,13 +3,14 @@ import sys
 
 
 def main(*argv):
-    l = len(sys.argv)
+    arg_len = len(sys.argv)
     sum = 0
-    if l > 1:
+    if arg_len > 1:
         for args in sys.argv:
             if args != sys.argv[0]:
                 sum = sum + int(args)
     print(sum)
+
 
 if __name__ == "__main__":
     main()
