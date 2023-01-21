d400997b9fa0a17734799047a29e8f013429add6
BrianOti <107025460+BrianOti@users.noreply.github.com>
2023-01-21 04:44:09 +0300

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-#pass a variable in the header of the request
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+
+# Check if the user has provided a URL
+if [ $# -eq 0 ]; then
+    echo "Error: No URL provided"
+    exit 1
+fi
+
+# Send a GET request to the URL using curl and display the body of the response while sending the header variable X-School-User-Id with the value 98
+curl -s -H "X-School-User-Id: 98" "$1"
