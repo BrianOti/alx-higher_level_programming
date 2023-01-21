b92ef93b355af73fb6caca62efd3f313be66e729
BrianOti <107025460+BrianOti@users.noreply.github.com>
2023-01-21 04:46:00 +0300

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if the user has provided a URL
-if [ $# -eq 0 ]; then
-    echo "Error: No URL provided"
-    exit 1
-fi
-
 # Send a GET request to the URL using curl and display the body of the response while sending the header variable X-School-User-Id with the value 98
 curl -s -H "X-School-User-Id: 98" "$1"
